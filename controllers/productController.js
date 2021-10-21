116d3d88d656267fde8b7b373e9524f0927b4e37
yassine ed-dyb <yassineeddyb@gmail.com>
2021-10-21 19:02:45 +0200

    adding the ability to get, update and delete product

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,8 +10,43 @@ exports.getAllProducts = async (req, res, next) => {
   res.status(200).json({ status: "success", products });
 };
 
+exports.getProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    next(new ApiError("there is no product with that id", 404));
+    return;
+  }
+
+  res.status(200).json({ status: "success", product });
+});
+
 exports.createProduct = catchAsync(async (req, res, next) => {
   const product = await Product.create(req.body);
 
   res.status(200).json({ status: "success", product });
+});
+
+exports.updateProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!product) {
+    next(new ApiError("there is no product with that id", 404));
+    return;
+  }
+
+  res.status(200).json({ status: "success", product });
+});
+
+exports.deleteProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+
+  if (!product) {
+    return next(new ApiError("there is no product with that id", 404));
+  }
+
+  res.status(200).json({ status: "success" });
 });
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -9,4 +9,10 @@ router
   .get(productController.getAllProducts)
   .post(productController.createProduct);
 
+router
+  .route("/:id")
+  .get(productController.getProduct)
+  .patch(productController.updateProduct)
+  .delete(productController.deleteProduct);
+
 module.exports = router;
