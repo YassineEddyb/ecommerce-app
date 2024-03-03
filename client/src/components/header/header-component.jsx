ccb2d92acaa5c37fd369a1e9e5697640466b7bc7
yassine ed-dyb <75098144+YassineEddyb@users.noreply.github.com>
2024-03-03 17:29:33 +0100

    Update header-component.jsx

--- client/src/components/header/header-component.jsx
+++ client/src/components/header/header-component.jsx
@@ -32,7 +32,7 @@ const Header = () => {
     <>
       <header className="header">
         <Link className="logo" to="/">
-          <h1>LOGO</h1>
+          <h1>STYILO</h1>
         </Link>
         <CategoryList />
         <nav>
